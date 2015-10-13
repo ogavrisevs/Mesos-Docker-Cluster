4f8b9783ac9c38d7e2526a0c31b74ed6048fb645
Oskars Gavriševs <Oskars.Gavrisevs@4finance.com>
2015-10-13 15:24:00 +0100

    Bump AWS ECS image version

--- docker/zookeeper/3.4.6/entrypoint.sh
+++ docker/zookeeper/3.4.6/entrypoint.sh
@@ -12,13 +12,15 @@ EOF
 
 IP="$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)"
 
-aws --region eu-west-1 ec2 describe-instances | jq -r -S ".Reservations[].Instances[] | select(contains( {Tags: [{ \"Value\": \"Mesos\", \"Key\": \"Project\"}]} )) | .PrivateIpAddress " > /tmp/zk-nodes
+aws --region eu-west-1 ec2 describe-instances | jq -r -S ".Reservations[].Instances[] | select(contains( {Tags: [{ \"Value\": \"Mesos\", \"Key\": \"Project\"}]} )) | select(contains( {State: { \"Code\": 16, \"Name\": \"running\" }} )) | .PrivateIpAddress " > /tmp/zk-nodes
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
     ID=`echo $line | cut -d \. -f 4`
-    echo "server.$ID=$line:2888:3888" >> /opt/zookeeper/conf/zoo.cfg
     if [ "$IP" == "$line" ]; then
         echo "$ID" > /opt/zookeeper/data/myid
+        echo "server.$ID=localhost:2888:3888" >> /opt/zookeeper/conf/zoo.cfg
+    else
+        echo "server.$ID=$line:2888:3888" >> /opt/zookeeper/conf/zoo.cfg
     fi
 done < /tmp/zk-nodes
 
